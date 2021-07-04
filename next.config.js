dfa00b84f9385fd05f5416aaef0a9813efee1274
Jaydeep <jaydeepshelake10@gmail.com>
2021-07-04 18:29:46 +0530

    changed to string

--- next.config.js
+++ next.config.js
@@ -3,6 +3,6 @@ module.exports={
         domains:['links.papareact.com',"fakestoreapi.com","images-eu.ssl-images-amazon.com"]
     },
     env:{
-        stripe_public_key:process.env.STRIPE_PUBLIC_KEY
+        stripe_public_key:`${process.env.STRIPE_PUBLIC_KEY}`
     }
 }
